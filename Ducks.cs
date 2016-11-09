2197ccfbae31b87cc5e5c365aa83cf518aa169ee
OlgaGerenko <gerenko_olga@ukr.net>
2016-11-09 16:38:43 +0300

    first commit

---
+++ Ducks.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AdapterConsole
+{
+	/// <summary>
+	/// Summary description for Duck.
+	/// </summary>
+	public interface Duck
+	{
+		string Quack();
+		string Fly();
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck turkey = new MallardDuck();
+            Console.WriteLine(turkey.Fly());
+            Console.WriteLine(turkey.Quack());
+           
+            WildTurkey wturkey = new WildTurkey();
+            Console.WriteLine(wturkey.Fly());
+            Console.WriteLine(wturkey.Gobble());
+            Console.ReadKey();
+        }
+    }
+}
